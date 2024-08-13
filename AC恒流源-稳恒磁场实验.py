c4bff07d12a2876db367a306140b88452eff7869
Winstion <123090611@link.cuhk.edu.cn>
2024-08-13 14:15:45 +0800

    8-13 wenfu

--- AC恒流源-稳恒磁场实验.py
+++ AC恒流源-稳恒磁场实验.py
@@ -1,35 +1,35 @@
 from lib.DG4202 import DG4202
 from lib.OE1022 import OE1022
+from lib.DP2031 import DP2031
 import time
 import os
 import numpy as np
 
 resource_name = 'USB0::0x1AB1::0x0641::DG4E195204310::INSTR'
 resource_name2 = 'USB0::0x1AB1::0x210B::DM8E260300157::INSTR'
+resource_name3 = 'USB0::0x1AB1::0xA4A8::DP2A242800127::INSTR'
+power_supply = DP2031(resource_name3)
 lock_in_amp = OE1022(resource_name2)
 generator = DG4202(resource_name)
 data = []
 amp_list = []
 
-#疑问1：reset()的默认设置到底是什么？它是否符合我们的测量要求？
-#疑问2：set_harmonic()的第一个参数是什么意思？
+# 设置锁相放大器的参数
 lock_in_amp.reset()
 lock_in_amp.set_harmonic(1,1)
 lock_in_amp.set_buffer_selection(1, 'Rh1')
 
-# 设置交变电流的幅值和步长，并转化为对应的交变电压的幅值
-for i in range(0, 41, 2):
-    amp = 6.03148 * i - 0.13831
-    amp_list.append(amp)
+# 设置交变恒流源提供1mA的交变电源
+amp = 7.7
+generator.apply_sine_wave(1, 3.14, amp , 0, 0)
+generator.set_output_state(1, 'ON')
 
-# 直接设置交变电压（mVpp）的幅值
-for amp in range(0,200,10):
-    amp_list.append(amp)
-
-for amp in amp_list:
-    generator.apply_sine_wave(1, 3.14, amp , 0, 0)
-    generator.set_output_state(1, 'ON')
-    time.sleep(5)
+# 调整DC励磁电流大小，并进行测量
+for current in range(0,101,5):
+    power_supply.set_voltage('CH3', 5)
+    power_supply.set_current('CH3', current*0.001)
+    power_supply.output_on()
+    time.sleep(10)
     lock_in_amp.get_start()
     time.sleep(3)
     lock_in_amp.stop()
@@ -37,7 +37,8 @@ for amp in amp_list:
     n = len(value)
     mean_value = sum(value) / n
     data.append(mean_value)
-    
+
+# 存储数据
 data_dir = '/Users/a1-6/VScode/Physic Experiment Game/Devices-Control/Data'
 file_path = os.path.join(data_dir, 'DC_交变磁场实验.csv')
 np.savetxt(file_path, data, delimiter=",")
