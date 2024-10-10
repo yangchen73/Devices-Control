d5c2a891d0dc14f3f3ccd5945a5f8676797ce00b
Winstion <123090611@link.cuhk.edu.cn>
2024-10-10 21:28:40 +0800

    10-10 wenfu

---
+++ Plot/锁相倍频采集试验.py
@@ -0,0 +1,16 @@
+import time
+from lib.OE1022 import OE1022
+
+lock_in_amp = OE1022('/dev/cu.usbserial-1120')  
+lock_in_amp.set_harmonic(1,1)
+lock_in_amp.set_harmonic(2,2)
+lock_in_amp.reset()
+lock_in_amp.set_buffer_selection(1, 'Rh1')
+lock_in_amp.set_buffer_selection(2, 'Rh2') 
+lock_in_amp.get_start()
+time.sleep(3)
+lock_in_amp.stop()
+value = lock_in_amp.read_buffer_data(1, 0, 50)
+print(lock_in_amp.calculate_statistics(value))
+value = lock_in_amp.read_buffer_data(2, 0, 50)
+print(lock_in_amp.calculate_statistics(value))
