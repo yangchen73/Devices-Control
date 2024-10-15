4eefaeb8761490ee585a11459953844dc6e73fd1
Winstion <123090611@link.cuhk.edu.cn>
2024-10-15 20:27:35 +0800

    1015

--- AC&AC实验.py
+++ AC&AC实验.py
@@ -26,24 +26,31 @@ for amp in range(2,83,2):
 generator.apply_sine_wave(1, 3.14, 13e-3, 0, 0)
 generator.set_output_state(1, 'ON')
 
-for amp in amp_list:
-    generator.apply_sine_wave(2, 3.14, amp , 0, 0)
-    generator.set_output_state(2, 'ON')
-    time.sleep(15)
-    lock_in_amp.reset()
-    lock_in_amp.set_harmonic(1,1)
-    lock_in_amp.set_buffer_selection(1, 'Rh1')
-    lock_in_amp.get_start()
-    time.sleep(3)
-    lock_in_amp.stop()
-    value = lock_in_amp.read_buffer_data(1, 0, 50)
-    # print(value)
-    # print(value[1:len(value)-1])
-    mean, variance, std_deviation = lock_in_amp.calculate_statistics(value[1:len(value)-1])
-    # print('mean=',mean, 'variance=',variance, 'std_deviation=',std_deviation)
-    print(f'Amplitude: {amp*1000} mVpp, Value: {mean*1000} mV')
-    data.append(float(mean*1000))
+n = 1
+for i in range(1, 3):
+    data.append(n*1000)
+    n = n + 1
+
+    for amp in amp_list:
+        generator.apply_sine_wave(2, 3.14, amp , 0, 0)
+        generator.set_output_state(2, 'ON')
+        time.sleep(10)
+        lock_in_amp.reset()
+        lock_in_amp.set_harmonic(1,1)
+        lock_in_amp.set_buffer_selection(1, 'Rh1')
+        lock_in_amp.get_start()
+        time.sleep(3)
+        lock_in_amp.stop()
+        value = lock_in_amp.read_buffer_data(1, 0, 50)
+        # print(value)
+        # print(value[1:len(value)-1])
+        mean, variance, std_deviation = lock_in_amp.calculate_statistics(value[1:len(value)-1])
+        # print('mean=',mean, 'variance=',variance, 'std_deviation=',std_deviation)
+        print(f'Amplitude: {amp*1000} mVpp, Value: {mean*1000} mV')
+        data.append(float(mean*1000))
+    
+    
     
 data_dir = '/Users/a1-6/VScode/Physic Experiment Game/Devices-Control/Data'
-file_path = os.path.join(data_dir, 'AC&AC实验.csv')
+file_path = os.path.join(data_dir, 'AC&AC实验 .csv')
 np.savetxt(file_path, data, delimiter=",")
--- AC&DC实验.py
+++ AC&DC实验.py
@@ -5,13 +5,13 @@ import time
 import os
 import numpy as np
 
-resource_name1 = 'USB0::0x1AB1::0x0641::DG4E182101266::INSTR'
-resource_name2 = '/dev/cu.usbserial-110'
+resource_name1 = 'USB0::0x1AB1::0x0641::DG4E182101276::INSTR'
+resource_name2 = '/dev/cu.usbserial-120'
 resource_name3 = 'USB0::0x1AB1::0xA4A8::DP2A242800127::INSTR'
 
 generator = DG4062(resource_name1)
 lock_in_amp = OE1022(resource_name2)
-power_supply = DP2031(resource_name3)
+# power_supply = DP2031(resource_name3)
 data = []
 amp_list = []
 data2 = []
@@ -22,10 +22,10 @@ generator.apply_sine_wave(1, 3.14, 13e-3 , 0, 0)
 generator.set_output_state(1, 'ON')
 
 # 调整DC励磁电流大小，并进行测量
-for current in range(0,41,1):
-    power_supply.set_voltage('CH2', 5)
-    power_supply.set_current('CH2', current*0.001)
-    power_supply.output_on()
+for current in range(1,41,1):
+    # power_supply.set_voltage('CH1', 5)
+    # power_supply.set_current('CH1', current*0.0001)
+    # power_supply.output_on()
     time.sleep(15)
     # 设置锁相放大器的参数
     lock_in_amp.reset()
@@ -36,7 +36,7 @@ for current in range(0,41,1):
     lock_in_amp.stop()
     value = lock_in_amp.read_buffer_data(1, 0, 50)
     mean, variance, std_deviation = lock_in_amp.calculate_statistics(value[1:len(value)-1])
-    print(f'Current: {current} mA, Value: {mean*1000} mV')
+    print(f'Current: {current*0.1} mA, Value: {mean*1000} mV')
     data.append(float(mean*1000))
 
 # generator.set_output_state(1, 'OFF')
